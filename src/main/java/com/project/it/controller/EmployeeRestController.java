30c7ae39a38b2bd81e15dbad4d25415ce0a2ecf1
Gabriel Faur <gfaur391@gmail.com>
2022-09-09 19:48:19 +0300

    Create Experience/EmployeeRestController

---
+++ src/main/java/com/project/it/controller/EmployeeRestController.java
@@ -0,0 +1,48 @@
+package com.project.it.controller;
+
+import com.project.it.entity.Employee;
+import com.project.it.service.EmployeeService;
+import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("employee")
+public class EmployeeRestController {
+
+    private final EmployeeService employeeService;
+
+
+    public EmployeeRestController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public List<Employee> getAllEmployee() { return employeeService.getAllEmployees(); }
+
+    @GetMapping("/{employeeId")
+    public Employee getEmployeeById(@PathVariable Long employeeId) {
+        return employeeService.getEmployeeById(employeeId);
+    }
+
+    @PostMapping
+    public Employee saveEmployee(@RequestBody Employee employee) {
+        return employeeService.saveEmployee(employee);
+    }
+
+    @PutMapping("/{employeeId")
+    public Employee updateEmployeeById(@RequestBody Employee employee, @PathVariable Long employeeId) {
+        return employeeService.updateEmployeeById(employee, employeeId);
+    }
+
+    @DeleteMapping("/{employeeId")
+    public void deleteEmployeeById(@PathVariable Long employeeId) {
+        employeeService.deleteEmployeeById(employeeId);
+    }
+}
