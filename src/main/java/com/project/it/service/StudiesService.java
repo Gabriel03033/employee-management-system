718d0cf0d929c0eb3f3d5d12f3f204735a0e5ad5
Gabriel Faur <gfaur391@gmail.com>
2022-09-09 14:34:29 +0300

    Define implementation for StudiesService to perform CRUD (create, read, update, delete) operations

--- src/main/java/com/project/it/service/StudiesService.java
+++ src/main/java/com/project/it/service/StudiesService.java
@@ -1,4 +1,12 @@
 package com.project.it.service;
 
+import com.project.it.entity.Studies;
+import java.util.List;
+
 public interface StudiesService {
+    List<Studies> getAllStudies();
+    Studies getStudiesById(Long studiesId);
+    Studies saveStudies(Studies studies);
+    Studies updateStudiesById(Studies studies, Long studiesId);
+    void deleteStudiesById(Long studiesId);
 }
