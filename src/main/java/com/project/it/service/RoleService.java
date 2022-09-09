a9a8657786b63abd8e6621c4871094a417297480
Gabriel Faur <gfaur391@gmail.com>
2022-09-09 14:35:12 +0300

    Define implementation for RoleService to perform CRUD (create, read, update, delete) operations

--- src/main/java/com/project/it/service/RoleService.java
+++ src/main/java/com/project/it/service/RoleService.java
@@ -1,4 +1,13 @@
 package com.project.it.service;
 
+import com.project.it.entity.Role;
+
+import java.util.List;
+
 public interface RoleService {
+    List<Role> getAllRoles();
+    Role getRoleById(Long rolesId);
+    Role saveRole(Role role);
+    Role updateRoleById(Role role, Long roleId);
+    void deleteRoleById(Long roleId);
 }
