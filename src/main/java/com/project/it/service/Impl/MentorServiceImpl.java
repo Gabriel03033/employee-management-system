6f41c16c45168bbd39061809a84bc96d4d7cfaf0
Gabriel Faur <gfaur391@gmail.com>
2022-09-09 14:55:57 +0300

    Define implementation for MentorService to perform CRUD (create, read, … …update, delete) operations (update)

---
+++ src/main/java/com/project/it/service/Impl/MentorServiceImpl.java
@@ -0,0 +1,46 @@
+package com.project.it.service.Impl;
+
+import com.project.it.entity.Mentor;
+import com.project.it.repository.MentorRepository;
+import com.project.it.service.MentorService;
+import java.util.List;
+
+public class MentorServiceImpl implements MentorService {
+
+    private final MentorRepository mentorRepository;
+
+    public MentorServiceImpl(MentorRepository mentorRepository) { this.mentorRepository = mentorRepository;}
+
+    @Override
+    public List<Mentor> getAllMentors() { return mentorRepository.findAll();}
+
+    @Override
+    public Mentor getMentorsById(Long mentorId) {
+        return mentorRepository.findById(mentorId).orElseThrow(() -> new RuntimeException("No mentors found with id: " + mentorId));}
+
+    @Override
+    public Mentor saveMentors(Mentor mentor) {
+        return mentorRepository.save(mentor);
+    }
+
+    @Override
+    public Mentor updateMentorsById(Mentor mentor, Long mentorId) {
+        Mentor mentorToUpdate = getMentorsById(mentorId);
+        mentorToUpdate.setMentorId(mentor.getMentorId());
+        mentorToUpdate.setFirstName(mentor.getFirstName());
+        mentorToUpdate.setLastName(mentor.getFirstName());
+        mentorToUpdate.setEmail(mentor.getEmail());
+        mentorToUpdate.setPassword(mentor.getPassword());
+        mentorToUpdate.setMobile(mentor.getMobile());
+        mentorToUpdate.setAddress(mentor.getAddress());
+        mentorToUpdate.setBirthday(mentor.getBirthday());
+        mentorToUpdate.setAvailable(mentor.getAvailable());
+        mentorToUpdate.setNumberOfEmployees(mentor.getNumberOfEmployees());
+        return mentorRepository.save(mentor);
+    }
+
+    @Override
+    public void deleteMentorsById(Long mentorId) {
+        mentorRepository.deleteById(mentorId);
+    }
+}
--- src/main/java/com/project/it/service/MentorService.java
+++ src/main/java/com/project/it/service/MentorService.java
@@ -1,4 +1,12 @@
 package com.project.it.service;
 
+import com.project.it.entity.Mentor;
+import java.util.List;
+
 public interface MentorService {
+    List<Mentor> getAllMentors();
+    Mentor getMentorsById(Long mentorId);
+    Mentor saveMentors(Mentor mentor);
+    Mentor updateMentorsById(Mentor mentor, Long mentorId);
+    void deleteMentorsById(Long mentorId);
 }
