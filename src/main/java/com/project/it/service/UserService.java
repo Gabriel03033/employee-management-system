9a07147fd2cbb7b5e55a941de214dea0c318c7cd
Gabriel Faur <gfaur391@gmail.com>
2022-09-09 13:55:29 +0300

    Create service interfaces + define implementation for UserService to perform CRUD (create, read, update, delete) operations

---
+++ src/main/java/com/project/it/service/UserService.java
@@ -0,0 +1,12 @@
+package com.project.it.service;
+
+import com.project.it.entity.User;
+import java.util.List;
+
+public interface UserService {
+    List<User> getAllUsers();
+    User getUserById(Long userId);
+    User saveUser(User user);
+    User updateUserById(User user, Long userId);
+    void deleteUserById(Long userId);
+}
