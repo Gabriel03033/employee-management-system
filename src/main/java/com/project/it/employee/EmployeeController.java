76c60dd2d143e8b0ac3a459ba578eb77958055b4
Gabriel Faur <gfaur391@gmail.com>
2022-09-11 17:55:22 +0300

    Create html page with form for updating an employee (UI)

--- src/main/java/com/project/it/employee/EmployeeController.java
+++ src/main/java/com/project/it/employee/EmployeeController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -33,5 +34,11 @@ public class EmployeeController {
     public String saveEmployee(@ModelAttribute EmployeeDto employeeDto) {
         employeeService.saveEmployee(employeeDto);
         return "redirect:/employees";
+    }
+
+    @GetMapping("/update-employee/{employeeId}")
+    public String viewUpdateEmployeeFormPage(Model model, @PathVariable Long employeeId) {
+        model.addAttribute("employeeDto", employeeService.getEmployeeById(employeeId));
+        return "update-employee";
     }
 }
