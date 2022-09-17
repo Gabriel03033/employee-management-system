6d1ff6f0cf7593588aa3f0e6f78c03fa1a800c68
Gabriel Faur <gfaur391@gmail.com>
2022-09-17 14:28:40 +0300

    Use thymeleaf fragments to outsource duplicated elements in the html pages

--- src/main/java/com/project/it/employee/EmployeeRepository.java
+++ src/main/java/com/project/it/employee/EmployeeRepository.java
@@ -1,16 +1,9 @@
 package com.project.it.employee;
 
-import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-   /* @Query("SELECT employee FROM Employee employee WHERE employee.name LIKE CONCAT('%', :name, '%')")
-    List<Employee> searchEmployeesByName(String name);*/
-
     Page<Employee> findByNameContaining(String searchedName, Pageable pageable);
 }
